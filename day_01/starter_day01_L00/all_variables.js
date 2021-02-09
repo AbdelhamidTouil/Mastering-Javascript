c9b284bf96f9300863d2778ea7f55f7445b76280
abdelhamidtouil <you@example.com>
2021-02-09 20:39:16 +0000

    secendcommmit

--- day_01/starter_day01_L00/all_variables.js
+++ day_01/starter_day01_L00/all_variables.js
@@ -1,1 +1,30 @@
 // *****Exercices: Data Types --> Level00 [6, 7, 8, 9, 10]*****
+//6
+
+
+var a ;
+var  b;
+var c;
+var d;
+
+
+//7
+var a=5 ;
+var  b="azery";
+var c=false;
+var d=0;
+
+//8 
+
+var nom="touil";
+var prenom="abdelhamid";
+var etat="azerty";
+var pays="maroc";
+var age=21;
+
+//9
+var nom="touil", prenom="abdelhamid", etat="azerty", pays="maroc", age=21;
+//10
+var myAge=21,yourAge=22;
+console.log( "myAge"+" "+myAge+"  "+"yourAge"+" "+yourAge);
+
