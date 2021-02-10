22fe320647c6439711f9723773f574187a15d4a1
abdelhamidtouil <you@example.com>
2021-02-10 13:35:42 +0000

    secondCommit

---
+++ day_01/starter_day01_L03/_boolean_L01.js
@@ -0,0 +1,8 @@
+//1
+// Déclarez et initialisez les variables: firstName, lastName, country, city, age, isMarried, year et utilisez 'typeof' operator pour vérifier les différents data types.
+var firstName="abdelhamid";
+var lastName="touil";
+var country="maroc";
+
+var age=21;
+var isMarried =false;
